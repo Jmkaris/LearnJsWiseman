9e99baffe9989fc982acf17c6c969e2be1a5bc7d
Jmkaris <mugwimikariuki101@gmail.com>
2025-09-29 15:42:24 +0300

    refactor: comment out initial JavaScript code and add CSS styles

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-console.log("Hello World");
-alert("Welcome to JavaScript");
+// console.log("Hello World");
+// alert("Welcome to JavaScript");
+// alert("Password confirmed");
+/* multiline
+ comment */
+//single line comment
+
+//prompt
+
+// let input1 = prompt("Enter your first name");
+// let input2 = Number(prompt("Enter your age"));
+
+// if (input1 === "James") {
+//   //James be a string
+//   alert("welcome James");
+// }
+
+// let inputx = Number(prompt("Enter the first number"));
+// let inputy = Number(prompt("Enter the first number"));
+
+// let sum = inputx + inputy;
+// console.log(sum);
+
+//Generating a random number
+
+let randomNumber = Math.floor(Math.random() * 100); //10 exclusive
+console.log(randomNumber);
