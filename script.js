73ce2c4a306c0fc364dfa0f2cd54398c0bc0fff0
Jmkaris <mugwimikariuki101@gmail.com>
2025-09-29 16:36:04 +0300

    refactor: comment out unused variable assignments and related code

--- script.js
+++ script.js
@@ -28,11 +28,24 @@
 
 // let myAge = Math.floor(Math.random() * 0.1);
 
-firstName = "James";
-firstName = "Karis";
-firstName = 45;
-firstline = true;
-firstName = "01bc";
-age = 33;
-AGE = 43;
-FIRSTNAME = "James";
+// firstName = "James";
+// firstName = "Karis";
+// firstName = 45;
+// firstline = true;
+// firstName = "01bc";
+// age = 33;
+// AGE = 43;
+// FIRSTNAME = "James";
+
+// let age = 45;
+// if (age === 45) {
+//   let age1 = 54;
+//   console.log(age);
+//   console.log(age1);
+// }
+// console.log(age);
+
+const gravity = 9.8;
+// gravity = 10;
+
+console.log(gravity);
